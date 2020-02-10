19c786284703843f40ea4d8fb8a2e3db6c96ebec
Fiona Wu <54760257+wufiona@users.noreply.github.com>
2020-02-10 11:55:27 -0600

    fixed

--- src/TwoDimensionalPoint.h
+++ src/TwoDimensionalPoint.h
@@ -3,101 +3,35 @@
  * Missouri State University, Fall 2019
  *
  * @author Jim Daehn <jdaehn@missouristate.edu>
- * @file   TwoDimensionalPoint.h
- * @brief  Specification of a TwoDimensionalPoint ADT.
+ * @file   Point.h
+ * @brief  Specification of a Point ADT.
  */
 
-#ifndef HW01_TWODIMENSIONALPOINT_H
-#define HW01_TWODIMENSIONALPOINT_H
+#ifndef HW01_POINT_H
+#define HW01_POINT_H
 
-#include "Point.h"
+using length_unit = double;
+using coordinate_unit = double;
 
 /**
- * An implementation of the Point interface in a two-dimensional plane.
+ * A Point Interface.
  */
-class TwoDimensionalPoint : public Point {
-private:
-    // TODO: Add appropriate data members
+class Point {
 public:
     /**
-     * Default/initializing constructor.
-     * @param x_coordinate the x-coordinate of this Point
-     * @param y_coordinate the y-coordinate of this Point
+     * Calculate the distance this Point is from the origin.
+     * @return The distance this Point is from the origin is returned.
      */
-    explicit TwoDimensionalPoint(coordinate_unit x_coordinate = 0, coordinate_unit y_coordinate = 0);
-
-    /* The Big Five */
+    virtual length_unit DistanceFromOrigin() const = 0;
 
     /**
-     * Destructor responsible for deleting memory occupied by the coordinates of this Point.
+     * Calculate the distance this Point is from the given Point.
+     * @param other another Point in the plane
+     * @return The distance this Point is from the given Point is returned.
      */
-    ~TwoDimensionalPoint() override;
+    virtual length_unit DistanceFromPoint(const Point &other) const = 0;
 
-    /**
-     * Copy constructor used to create a new Point with the same coordinates of the other Point.
-     * @param other a Point used as a template for creating this point using copy semantics
-     */
-    TwoDimensionalPoint(const TwoDimensionalPoint &other);
-
-    /**
-     * Move constructor used to create a new Point with the same coordinates of the other Point.
-     * @param other a Point used as a template for creating this point using move semantics
-     */
-    TwoDimensionalPoint(TwoDimensionalPoint &&other) noexcept;
-
-    /**
-     * Copy assignment operator used to create a new Point with the same coordinates as the other Point.
-     * @param rhs an l-value; the right-hand side of the assignment statement lhs = rhs
-     * @return A new Point with the same coordinates as the given (l-value) TwoDimensionalPoint is returned.
-     */
-    TwoDimensionalPoint &operator=(const TwoDimensionalPoint &rhs);
-
-    /**
-     * Move assignment operator used to create a new Point with the same coordinates as the other Point.
-     * @param rhs an r-value; the right-hand side of the assignment statement TwoDimensionalPoint lhs = TwoDimensionalPoint{x, y}
-     * @return A new Point with the same coordinates as the given (r-value) TwoDimensional Point is returned.
-     */
-    TwoDimensionalPoint &operator=(TwoDimensionalPoint &&rhs);
-
-    /* Inherited Point behavior */
-
-    /**
-     * @copydoc Point::DistanceFromOrigin() const
-     */
-    length_unit DistanceFromOrigin() const override;
-
-    /**
-     * @copydoc Point::DistanceFromPoint(const Point &other) const
-     */
-    length_unit DistanceFromPoint(const Point &other) const override;
-
-    /* Specific TwoDimensionalPoint behavior */
-
-    /**
-     * X-coordinate accessor method.
-     * @return The x-coordinate of this Point is returned.
-     */
-    virtual length_unit GetX() const;
-
-    /**
-     * Y-coordinate accessor method.
-     * @return The y-coordinate of this Point is returned.
-     */
-    virtual length_unit GetY() const;
-
-    /**
-     * Assess whether this Point is the origin.
-     * @return True if this Point is the origin, otherwise false.
-     */
-    virtual bool IsOrigin() const;
-
-    /**
-     * Translates this Point by the given dimensions.
-     * @param x the amount to translate in the x-direction
-     * @param y the amount to translate in the y-direction
-     */
-    void Translate(coordinate_unit x = 0, coordinate_unit y = 0);
+    virtual ~Point() = default;
 };
 
-
-#endif //HW01_TWODIMENSIONALPOINT_H
+#endif //HW01_POINT_H
